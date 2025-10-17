c518a85a2ef48319d1f1b21ced88106f2b27981e
kathyacastro006 <kcastro52006@gmail.com>
2025-10-17 14:15:03 -0600

    Update ejercicio A.py

--- ejercicio A.py
+++ ejercicio A.py
@@ -1,13 +1,17 @@
-correo = input("escriba su correo electrónico: ")
-
-print(correo.count("@") ==1 and correo.find("@")> 3 and "." in correo and " " not in correo and correo[0] != "." and correo[-1] != ".")
+correo = input()
+print(correo.count("@") ==1
+and correo.index("@")>= 3 and 
+"." in correo 
+and " " not in correo 
+and correo[0] != "." 
+and correo[-1] != ".")
 
 
 
 #El correo contiene exactamente un @: correo.count("@") ==1
 
-#Antes y después del @ debe haber al menos 3 caracteres: correo.find("@")> 3
-#"find" sirve para encontrar la posición de un caracter dentro del input. eso de ahí significa "significa “¿la posición del @ es mayor que 3 caracteres?”"
+#Antes y después del @ debe haber al menos 3 caracteres: correo.index("@")>= 3
+#index te devuelve en que posición está el caracter que pongas, es este caso el @, o sea lo busca y te dice que hay 3 o más caracteres antes de él
 
 #El correo debe contener al menos un punto: "." in correo
 
